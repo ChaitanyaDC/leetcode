d52d02c68ebeab44fe4603d51c7934913d74b8fd
Chaitanya Dayanand Chaudhari <chaitanyadchaudhari@gmail.com>
2024-02-02 18:02:03 -0800

    Test program to leetcode with C#

---
+++ c#/leetcode/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using leetcode.challenges._217ContainsDuplicate;
+
+Console.WriteLine("Hello, World!");
+_217ContainsDuplicate.ContainsDuplicate();
---
+++ c#/leetcode/challenges/217ContainsDuplicate.cs
@@ -0,0 +1,15 @@
+namespace leetcode.challenges._217ContainsDuplicate;
+
+public static class _217ContainsDuplicate {
+    public static void ContainsDuplicate() {
+        // HashSet<int> dictNum = new HashSet<int>();
+        // foreach (int i in nums) {
+        //     if (dictNum.Contains(i)) {
+        //         return true;
+        //     }
+        //     dictNum.Add(i);
+        // }
+        // return false;
+        Console.WriteLine("Inside the Contains duplicate class");
+    }
+}
